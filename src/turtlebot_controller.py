948075e2e8919dc5beb168454b52608729eb5a5e
shantanuparabumd <sparab@umd.edu>
2023-04-06 12:02:07 -0400

    Launch file Corrections

--- src/turtlebot_controller.py
+++ src/turtlebot_controller.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-
 import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Float64
+from std_msgs.msg import Bool
 from geometry_msgs.msg import Twist
 from controller_manager_msgs.srv import ListControllers, ListControllersRequest
 from controller_manager_msgs.srv import SwitchController, SwitchControllerRequest
@@ -10,52 +10,73 @@ import math
 import os
 import rospkg
 
-def publisher(states):
-    t = Twist()
-    pub_move = rospy.Publisher('/cmd_vel', Twist, queue_size=10) 
-    rospy.init_node('astar_publisher', anonymous=True)
-    rate = rospy.Rate(1)
-    rate_ = 1
-    wheel_radius= 0.033
-    wheel_distance= 0.16
-    theta = 0
-    x_vel = 0
-    y_vel = 0
-    ang_vel = 0
+class Robot:
 
-    for i in range(5):
-        rate.sleep()
-
-    for right_rpm,left_rpm in states:
-
-        # left_velocity = left_rpm * wheel_radius * 2 * 3.14 / 60
-        # right_velocity = right_rpm * wheel_radius * 2 * 3.14 / 60
-
-        # # Compute the robot's linear and angular velocities
-        # x_vel = (left_velocity + right_velocity) / 2
-        # ang_vel = (right_velocity - left_velocity) / wheel_distance
+    def __init__(self):
+        rospy.init_node('astar_publisher', anonymous=True)
+        self.t = Twist()
+        self.pub_move = rospy.Publisher('/cmd_vel', Twist, queue_size=10) 
+        rospy.Subscriber("startflag", Bool, self.callback)
+        self.rate = rospy.Rate(1)
+        self.rate_ = 1
+        self.wheel_radius= 0.033
+        self.wheel_distance= 0.16
+        self.flag=False
         
-        ang_vel =(wheel_radius/wheel_distance)*(right_rpm - left_rpm)
-        x_vel = 0.5*wheel_radius*(right_rpm+left_rpm)*math.cos(theta*3.14/180)
-        y_vel = 0.5*wheel_radius*(right_rpm+left_rpm)*math.sin(theta*3.14/180)
-        x_robot = (x_vel**2 + y_vel**2)**0.5
 
-        for i in range(10):
-            theta = theta + ang_vel*0.1
-        theta = theta%(math.pi)
-        # data_str = "Pubishing Commands to Topics, " + str(x_vel) + " , "+ str(ang_vel)+ ","+str(theta)
-        # rospy.loginfo(data_str)
-        print(str(x_robot), " , ", str(ang_vel), ","+str(theta), ",", left_rpm, right_rpm)
-        t.angular.z = rate_*ang_vel/1.6
-        t.linear.x = x_robot/7.0
-        pub_move.publish(t)
-        rate.sleep()
+    def publisher(self,states):
+        
+        theta = 0
+        x_vel = 0
+        y_vel = 0
+        ang_vel = 0
+        if self.flag==True:
+
+            for i in range(5):
+                self.rate.sleep()
+
+            for right_rpm,left_rpm in states:
+
+                # left_velocity = left_rpm * wheel_radius * 2 * 3.14 / 60
+                # right_velocity = right_rpm * wheel_radius * 2 * 3.14 / 60
+
+                # # Compute the robot's linear and angular velocities
+                # x_vel = (left_velocity + right_velocity) / 2
+                # ang_vel = (right_velocity - left_velocity) / wheel_distance
+                
+                ang_vel =(self.wheel_radius/self.wheel_distance)*(right_rpm - left_rpm)
+                x_vel = 0.5*self.wheel_radius*(right_rpm+left_rpm)*math.cos(theta*3.14/180)
+                y_vel = 0.5*self.wheel_radius*(right_rpm+left_rpm)*math.sin(theta*3.14/180)
+                x_robot = (x_vel**2 + y_vel**2)**0.5
+
+                for i in range(10):
+                    theta = theta + ang_vel*0.1
+                theta = theta%(math.pi)
+                # data_str = "Pubishing Commands to Topics, " + str(x_vel) + " , "+ str(ang_vel)+ ","+str(theta)
+                # rospy.loginfo(data_str)
+                print(str(x_robot), " , ", str(ang_vel), ","+str(theta), ",", left_rpm, right_rpm)
+                self.t.angular.z = self.rate_*ang_vel/1.6
+                self.t.linear.x = x_robot/7.0
+                self.pub_move.publish(t)
+                self.rate.sleep()
+
+    def callback(self,msg):
+        
+        if msg.data==True:
+            self.flag=True
+        else:
+            self.flag=False
+
 
 if __name__ == '__main__':
 
-    rospy.loginfo("Waiting for path to be generated")
+    
+    r=Robot()
     read = False
     states = []
+    while(r.flag==False):
+        # rospy.loginfo("Waiting for path to be generated")
+        pass
     while (read == False):
         try:
             # print('Inside try')
@@ -77,8 +98,8 @@ if __name__ == '__main__':
         except:
             continue
     states.append((0,0))
-
+    
     try:
-        publisher(states)
+        r.publisher(states)
     except rospy.ROSInterruptException:
         pass
